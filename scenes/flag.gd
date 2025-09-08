015e05b07097576ce4e7b6d6fe8821b2744415aa
Matt Giuca <matt.giuca@gmail.com>
2025-09-08 23:13:29 +1000

    Added flag on top of keyhole.
    The flag can pop up to show the state of the keyhole.

---
+++ scenes/flag.gd
@@ -0,0 +1,32 @@
+@tool
+class_name Flag
+extends Node3D
+
+const ANIM_TIME : float = 0.3
+
+@onready var pivot : Node3D = $Pivot
+@onready var sprite : Sprite3D = %Sprite
+
+@export var texture : Texture2D:
+  set(value):
+    texture = value
+    if sprite != null:
+      sprite.texture = texture
+
+var tween : Tween
+
+@export var up : bool:
+  set(value):
+    up = value
+    # TODO: Tween it quickly.
+    if tween:
+      tween.kill()
+    tween = get_tree().create_tween()
+    tween.set_ease(Tween.EASE_OUT)
+    # Spring up, bounce down.
+    tween.set_trans(Tween.TRANS_SPRING if up else Tween.TRANS_BOUNCE)
+    tween.tween_property(pivot, 'rotation',
+      Vector3(TAU/4 if up else 0.0, 0.0, 0.0), ANIM_TIME)
+
+func _ready() -> void:
+  sprite.texture = texture

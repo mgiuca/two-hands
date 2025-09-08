2ab5371bde05f96f9d88fa639df4c209583d241d
Matt Giuca <matt.giuca@gmail.com>
2025-09-08 18:51:41 +1000

    Snap-lock key into position at the keyhole.
    When key is near the keyhole, its position and rotation are locked, and
    it can only rotate freely in a single axis.

--- scenes/key.gd
+++ scenes/key.gd
@@ -4,7 +4,30 @@ extends AnimatableBody3D
 ## Controller this key is bound to. Can't be null.
 var xr_controller : XRController3D
 
+## Keyhole this is currently inside of (position and semi-rotation-locked),
+## or null in the usual state of floating.
+var lock_to_keyhole : Keyhole
+
+@onready var visual : Node3D = $Visual
+
 func _physics_process(_delta: float) -> void:
   # Follow the controller around. (This has to be done manually, not via
   # the parenting system, or collision detection won't work properly.)
+  # NOTE: This happens even when the key is apparently locked in the keyhole,
+  # the physical key still moves (that way, we can detect when it leaves the
+  # keyhole!). Only the model gets locked. We transform the model separately,
+  # as it has top_level = true.
   global_transform = xr_controller.global_transform
+
+  if lock_to_keyhole:
+    # Snap the key's position to the keyhole. Only take a single axis of
+    # rotation from the controller.
+    visual.global_position = lock_to_keyhole.key_lock_global_position
+    var aligned_rotation := Vector3.ZERO
+    # Only take Z axis rotation from the controllers.
+    # TODO: Allow the keyhole to determine the allowed rotation axis and the
+    # fixed rotation in the other two axes.
+    aligned_rotation.z = xr_controller.global_rotation.z
+    visual.global_rotation = aligned_rotation
+  else:
+    visual.global_transform = global_transform
--- scenes/keyhole.gd
+++ scenes/keyhole.gd
@@ -1,8 +1,19 @@
 class_name Keyhole
 extends Node3D
 
+@onready var key_lock_marker : Marker3D = $KeyLockMarker
+
+var key_lock_global_position : Vector3:
+  get():
+    return key_lock_marker.global_position
+
 func _on_collision_area_body_entered(body: Node3D) -> void:
-  print('%s entered keyhole' % body)
+  if body is Key:
+    var key := body as Key
+    key.lock_to_keyhole = self
 
 func _on_collision_area_body_exited(body: Node3D) -> void:
-  print('%s exited keyhole' % body)
+  if body is Key:
+    var key := body as Key
+    if key.lock_to_keyhole == self:
+      key.lock_to_keyhole = null

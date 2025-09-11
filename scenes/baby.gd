0faf34c9c74a9acefd09bb0aeb6c7b00daec0357
Matt Giuca <matt.giuca@gmail.com>
2025-09-11 17:39:07 +1000

    Babies level: added crying sounds.

--- scenes/baby.gd
+++ scenes/baby.gd
@@ -6,6 +6,14 @@ extends Activator
 @onready var snd_insert : AudioStreamPlayer3D = $SndInsert
 @onready var snd_remove : AudioStreamPlayer3D = $SndRemove
 
+@export var crying_sound : AudioStream:
+  set(value):
+    crying_sound = value
+    if snd_crying:
+      snd_crying.stream = value
+
+@onready var snd_crying : AudioStreamPlayer3D = $SndCrying
+
 var bottle_lock_global_transform : Transform3D:
   get():
     return bottle_lock_marker.global_transform
@@ -23,6 +31,10 @@ var bottle_angle : float:
     # Set bottlehole activate iff the bottle is turned far enough.
     active = absf(bottle_angle) >= TAU/4 - 0.01
 
+func _ready() -> void:
+  snd_crying.stream = crying_sound
+  snd_crying.play()
+
 func _on_collision_area_body_entered(body: Node3D) -> void:
   if body is Bottle and inserted_bottle == null:
     inserted_bottle = body as Bottle
@@ -36,3 +48,13 @@ func _on_collision_area_body_exited(body: Node3D) -> void:
     inserted_bottle = null
     active = false
     snd_remove.play()
+
+func _on_snd_crying_finished() -> void:
+  # Just keep crying.
+  snd_crying.play()
+
+func _on_activate() -> void:
+  snd_crying.stop()
+
+func _on_deactivate() -> void:
+  snd_crying.play()
